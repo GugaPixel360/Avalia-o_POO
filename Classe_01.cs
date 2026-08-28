348fbc3be06965486793ca77480d8fc6889a6d01
GugaPixel360 <gmadureira198@gmail.com>
2026-08-28 08:50:51 -0300

    quase class 1

--- Classe_01.cs
+++ Classe_01.cs
@@ -1,4 +1,24 @@
-class CarrinhoDeCompras
+class PlayList
 {
-    private List<Lista> Lista
-}
+    private List<Musica> musicas;
+
+    private string? nomemusica;
+
+    public string NomeMusica
+    {
+        get { return nomemusica; }
+        set { nomemusica = value; }
+    } 
+
+    public List<Musica> Musicas
+    {
+        get { return musicas; }
+        set { musicas = value; }
+    }
+
+    public PlayList(string nomeMusica)
+    {
+        this.nomemusica = nomeMusica;
+        this.musicas = new List<Musica>();
+    }
+}   
